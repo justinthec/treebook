9f0118bf7441950bd32cd2ee310025ecbd926763
Justin Chan <justin.the.c@gmail.com>
2014-07-18 03:57:48 -0400

    Added tests for status authentication and validation

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,14 @@
 class Status < ActiveRecord::Base
   attr_accessible :content, :user_id
   belongs_to :user
+
+  validates :content, presence: true,
+  					  length: {minimum: 2}
+  					  # format: {
+  					  	# with: /\A..+\Z/,
+  					  	# message: "must be at least 2 characters"
+  					  # } R.I.P. My Regex child
+
+  validates :user_id, presence: true
+
 end
--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "that a status requires content" do
+  	status = Status.new
+  	assert !status.save
+  	assert !status.errors[:content].empty?
+  end
+
+  test "that a status requires content of at least 2 letters long" do
+  	status = Status.new
+  	status.content = "a"
+  	assert !status.save
+  	assert !status.errors[:content].empty?
+  end
+
+  test "that a status requires a user id" do
+  	status = Status.new
+  	status.content = "apple"
+  	assert !status.save
+  	assert !status.errors[:user_id].empty?
+  end
 end
