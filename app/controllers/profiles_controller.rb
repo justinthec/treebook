cefe26c88628867be6af61a869c9fff80cac338e
Justin Chan <justin.the.c@gmail.com>
2014-07-19 03:08:05 -0400

    Integrated profiles and included tests for them

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,12 @@
 class ProfilesController < ApplicationController
   def show
+  	@user = User.find_by_profile_name(params[:id])
+  	if @user
+  		@statuses = @user.statuses.all
+  	  # @statuses = Status.find_all_by_user_id(@user.id) # R.I.P. My bootiful solution
+	  render action: :show
+  	else
+	  render file: 'public/404', status: 404, formats: [:html]
+  	end
   end
 end
